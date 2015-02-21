f887e078b370dfca4874d5f8d782fca042deaaa2
Jose Vargas Molina <zardonkan@gmail.com>
2015-02-20 21:48:08 -0300

    configuración y directorio Setting

--- sitio/settings.py
+++ sitio/settings.py
@@ -89,4 +89,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static-comun'), )
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
+
+### Cache
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache/'),
+    }
+}
