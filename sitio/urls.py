c9660fb0a2d8eb2c9837ea2c26d35b64ce61358a
Jose Vargas Molina <zardonkan@gmail.com>
2015-02-20 22:34:33 -0300

    config urls.py

--- sitio/urls.py
+++ sitio/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +10,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'sitio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),   
+
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('', 
+
 )
