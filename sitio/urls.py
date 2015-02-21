e82967d05925bd0fcf91b5d91bf8bbefbc10c3d8
Jose Vargas Molina <zardonkan@gmail.com>
2015-02-20 23:39:09 -0300

    modificación static y media  directorio

--- sitio/urls.py
+++ sitio/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
 from django.contrib import admin
@@ -9,12 +10,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sitio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,}),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),   
-
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += patterns('', 
 
