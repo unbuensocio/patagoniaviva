74c3b0c7b5e4ae9a82fb5e8945077336291f8834
Jose Vargas Molina <zardonkan@gmail.com>
2015-02-20 21:24:44 -0300

    Arreglando hora para Chile

--- sitio/settings.py
+++ sitio/settings.py
@@ -75,9 +75,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es_CL'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
