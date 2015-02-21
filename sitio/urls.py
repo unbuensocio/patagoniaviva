0f17d58afa1c2b17dfcaccf7e5901bf7496f6d48
Jose Vargas Molina <zardonkan@gmail.com>
2015-02-20 23:56:56 -0300

    Revert "config urls.py"
    This reverts commit c9660fb0a2d8eb2c9837ea2c26d35b64ce61358a.
    
    Conflicts:
    	sitio/urls.py

--- sitio/urls.py
+++ sitio/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +10,3 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += patterns('', 
-
-)
