84b55146a02beb3529683f35cbd69653f0472702
Jose Vargas Molina <zardonkan@gmail.com>
2015-02-20 21:17:14 -0300

    Agregado Base datos MYSQL

--- sitio/settings.py
+++ sitio/settings.py
@@ -55,12 +55,22 @@ WSGI_APPLICATION = 'sitio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'sitio/database.cnf'),
+        },
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
